7c5ff107563493d2bfaaba12e7de0aa162512a81
CorentinBorges <56678169+CorentinBorges@users.noreply.github.com>
2020-11-18 15:57:19 +0100

    fix some phpcs

--- src/Validator/Voters/IsAdminVoter.php
+++ src/Validator/Voters/IsAdminVoter.php
@@ -1,11 +1,8 @@
 <?php
 
-
 namespace App\Validator\Voters;
 
-
 use App\Entity\Client;
-use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
